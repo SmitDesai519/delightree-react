bcfa8550568f2f46444a12217c66e3729eb5696c
Smit Desai <desaismit1594@gmail.com>
2024-04-06 17:18:21 +0530

    User details UI

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -1,6 +1,17 @@
-import { Card, CardHeader, Heading, Input, Stack } from "@chakra-ui/react";
+import {
+  Card,
+  FormControl,
+  FormLabel,
+  Heading,
+  Icon,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { Select, chakraComponents } from "chakra-react-select";
+import { CheckboxIcon } from "@chakra-ui/react";
 import "./UserDetails.css";
+import React from "react";
 
 type FormInputs = {
   firstName: string;
@@ -12,6 +23,32 @@ type FormInputs = {
   phonenNumber: number;
 };
 
+const genderOptions = [
+  {
+    value: "male",
+    label: "Male",
+  },
+  {
+    value: "female",
+    label: "Female",
+  },
+  {
+    value: "other",
+    label: "Other",
+  },
+];
+
+const customSelectComponents = {
+  Option: ({ children, ...props }: { children: React.ReactNode }) => (
+    <chakraComponents.Option {...props}>
+      {children}
+      {/* <Box ml="auto"> */}
+      <CheckboxIcon color="green.500" />
+      {/* </Box> */}
+    </chakraComponents.Option>
+  ),
+};
+
 export const UserDetails = () => {
   const {
     register,
@@ -20,16 +57,43 @@ export const UserDetails = () => {
     formState: { errors },
   } = useForm<FormInputs>();
   const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="container">
       <Heading as="h1" size="md">
         User Details
       </Heading>
       <Card variant="outline" p={4}>
-        <CardHeader fontWeight={500}>Basic Details</CardHeader>
-        <Stack direction={{ xs: "column", md: "row" }}>
-          <Input variant="outline" placeholder="First Name*" />
-          <Input variant="outline" placeholder="Last Name*" />
+        <Heading as="h6" size="xs" mb={2}>
+          Basic Details
+        </Heading>
+        <Stack direction={["column", "row"]}>
+          <FormControl isRequired>
+            <FormLabel>First name</FormLabel>
+            <Input variant="outline" placeholder="Enter First Name" />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Last name</FormLabel>
+            <Input variant="outline" placeholder="Enter Last Name*" />
+          </FormControl>
+        </Stack>
+        <Heading as="h6" size="xs" mb={2} mt={4}>
+          Other Information
+        </Heading>
+        <Stack direction={["column", "row"]}>
+          <FormControl isRequired>
+            <FormLabel>Gender</FormLabel>
+            <Select
+              name="gender"
+              options={genderOptions}
+              placeholder="Select Gender"
+              components={customSelectComponents}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Date of Birth</FormLabel>
+            <Input placeholder="Select Date" size="md" type="date" />
+          </FormControl>
         </Stack>
       </Card>
     </form>
