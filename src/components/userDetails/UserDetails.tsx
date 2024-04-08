23eeaf8104e522a20badcad5e75f5b4e7c63fe03
Smit Desai <desaismit1594@gmail.com>
2024-04-08 20:19:47 +0530

    User details UI

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -1,26 +1,33 @@
 import {
+  Box,
+  Button,
   Card,
+  Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
-  Icon,
+  IconButton,
   Input,
   Stack,
 } from "@chakra-ui/react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import { Select, chakraComponents } from "chakra-react-select";
-import { CheckboxIcon } from "@chakra-ui/react";
+import { FaCheck } from "react-icons/fa";
 import "./UserDetails.css";
 import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { RiAddLine, RiCloseLine } from "react-icons/ri";
 
 type FormInputs = {
   firstName: string;
   lastName: string;
   gender: string;
   dob: Date;
-  texhStack: string[];
-  email: string;
-  phonenNumber: number;
+  techStacks: string[];
+  email?: string;
+  phonenNumber?: number;
 };
 
 const genderOptions = [
@@ -39,24 +46,75 @@ const genderOptions = [
 ];
 
 const customSelectComponents = {
-  Option: ({ children, ...props }: { children: React.ReactNode }) => (
+  Option: ({
+    children,
+    isSelected,
+    ...props
+  }: {
+    children: React.ReactNode;
+    isSelected: boolean;
+  }) => (
     <chakraComponents.Option {...props}>
       {children}
-      {/* <Box ml="auto"> */}
-      <CheckboxIcon color="green.500" />
-      {/* </Box> */}
+      {isSelected && (
+        <Box ml="auto">
+          <FaCheck color="green" />
+        </Box>
+      )}
     </chakraComponents.Option>
   ),
 };
+
+const schema = yup.object().shape({
+  firstName: yup
+    .string()
+    .required("First name is required")
+    .matches(/^[a-zA-Z]+$/, "First name is incorrect"),
+  lastName: yup
+    .string()
+    .required("Last name is required")
+    .matches(/^[a-zA-Z]+$/, "Last name is incorrect"),
+  gender: yup.object().required("Gender is required"),
+  dob: yup
+    .date()
+    .typeError("Invalid date")
+    .required("Date of birth is required"),
+  techStacks: yup
+    .array()
+    .of(yup.string().required("Tech stack is required"))
+    .min(1, "At least one tech stack is required"),
+  // email: string;
+  // phonenNumber: number;
+});
 
 export const UserDetails = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    getValues,
+    setValue,
+    control,
     formState: { errors },
-  } = useForm<FormInputs>();
+  } = useForm<FormInputs>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      techStacks: [""], // Initialize with one empty stack
+    },
+  });
   const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
+  const addStack = () => {
+    const currentStacks = getValues("techStacks");
+    setValue("techStacks", [...currentStacks, ""]);
+  };
+
+  const removeStack = (indexToRemove: number) => {
+    const currentStacks = getValues("techStacks");
+    setValue(
+      "techStacks",
+      currentStacks.filter((_, index) => index !== indexToRemove)
+    );
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="container">
@@ -68,33 +126,112 @@ export const UserDetails = () => {
           Basic Details
         </Heading>
         <Stack direction={["column", "row"]}>
-          <FormControl isRequired>
-            <FormLabel>First name</FormLabel>
-            <Input variant="outline" placeholder="Enter First Name" />
+          <FormControl isInvalid={!!errors.firstName?.message}>
+            <FormLabel>
+              First name <span className="error">*</span>
+            </FormLabel>
+            <Input
+              {...register("firstName")}
+              variant="outline"
+              placeholder="Enter First Name"
+            />
+            {errors.firstName?.message && (
+              <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
+            )}
           </FormControl>
-          <FormControl isRequired>
-            <FormLabel>Last name</FormLabel>
-            <Input variant="outline" placeholder="Enter Last Name*" />
+          <FormControl isInvalid={!!errors.lastName?.message}>
+            <FormLabel>
+              Last name <span className="error">*</span>
+            </FormLabel>
+            <Input
+              variant="outline"
+              placeholder="Enter Last Name"
+              {...register("lastName")}
+            />
+            {errors.lastName?.message && (
+              <FormErrorMessage>{errors.lastName?.message}</FormErrorMessage>
+            )}
           </FormControl>
         </Stack>
         <Heading as="h6" size="xs" mb={2} mt={4}>
           Other Information
         </Heading>
         <Stack direction={["column", "row"]}>
-          <FormControl isRequired>
-            <FormLabel>Gender</FormLabel>
-            <Select
-              name="gender"
-              options={genderOptions}
-              placeholder="Select Gender"
-              components={customSelectComponents}
+          <Controller
+            control={control}
+            name="gender"
+            render={({ field: { onChange, value } }) => (
+              <FormControl isInvalid={!!errors.gender?.message}>
+                <FormLabel>
+                  Gender <span className="error">*</span>
+                </FormLabel>
+                <Select
+                  options={genderOptions}
+                  placeholder="Select Gender"
+                  components={customSelectComponents}
+                  value={value}
+                  onChange={onChange}
+                />
+                {errors.gender?.message && (
+                  <FormErrorMessage>{errors.gender?.message}</FormErrorMessage>
+                )}
+              </FormControl>
+            )}
+          />
+          <FormControl isInvalid={!!errors.dob?.message}>
+            <FormLabel>
+              Date of Birth <span className="error">*</span>
+            </FormLabel>
+            <Input
+              placeholder="Select Date"
+              size="md"
+              type="date"
+              max={new Date().toISOString().split("T")[0]}
+              {...register("dob")}
             />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel>Date of Birth</FormLabel>
-            <Input placeholder="Select Date" size="md" type="date" />
+            {errors.dob?.message && (
+              <FormErrorMessage>{errors.dob?.message}</FormErrorMessage>
+            )}
           </FormControl>
         </Stack>
+        <Stack spacing={4}>
+          {getValues("techStacks").map((_, index) => (
+            <Flex key={index}>
+              <Controller
+                name={`techStacks[${index}]`}
+                control={control}
+                defaultValue=""
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    value={value}
+                    onChange={onChange}
+                    placeholder="Tech Stack"
+                  />
+                )}
+              />
+              {index > 0 && (
+                <IconButton
+                  icon={<RiCloseLine />}
+                  aria-label="Remove"
+                  onClick={() => removeStack(index)}
+                />
+              )}
+            </Flex>
+          ))}
+          <IconButton
+            icon={<RiAddLine />}
+            aria-label="Add"
+            onClick={addStack}
+          />
+        </Stack>
+        <Button
+          colorScheme="teal"
+          size="sm"
+          type="submit"
+          className="submitbtn"
+        >
+          Submit
+        </Button>
       </Card>
     </form>
   );
